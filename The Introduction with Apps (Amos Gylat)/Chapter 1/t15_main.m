a990d7f33114e771f96f399a97287bd89c328c44
DymOK <dym93ok@yandex.ru>
2020-10-04 01:34:45 +0300

    К задачам добавлены условия

--- The Introduction with Apps (Amos Gylat)/Chapter 1/t15_main.m
+++ The Introduction with Apps (Amos Gylat)/Chapter 1/t15_main.m
@@ -1,3 +1,7 @@
+% Дано: I - интеграл,
+% I [x * sin(a * x) * dx] = sin(a * x) / a^2 - x * cos(a * x) / a
+% Вычислить определённый интеграл I(pi / 3, 3 * pi / 2)[x * sin(0.6 * x) * dx]
+
 a = 0.6;
 upper_bound = 3 * pi / 2;
 lower_bound = pi / 3;
